ad8582b26ceb9794ca99014b23c4c3652c893891
SapoCharmoso <charmyfrogy@gmail.com>
2025-07-01 19:38:41 -0300

    Descartei as alterações do projeto padrão do React e adicionei o layout da todo list, alem de ter começado a logica de adicionar as tarefas

--- to-do_list/src/App.jsx
+++ to-do_list/src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./styles.css";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const [newItem, setNewItem] = useState("");
+  const [todos, setTodos] = useState([]);
 
+  // terminar de fazer o handleSubmit
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    setTodos(...[todos], {
+      id: crypto.randomUUID,
+      title: newItem,
+      completed: false,
+    });
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit} className="item-form">
+        <div className="form-row">
+          <label htmlFor="item">Novo Item</label>
+          <input
+            value={newItem}
+            onChange={(e) => setNewItem(e.target.value)}
+            type="text"
+          />
+        </div>
+        <button className="btn-add">Add</button>
+      </form>
+      <h1 className="">Todo List</h1>
+      <ul>
+        <li>
+          <label htmlFor="">
+            <input type="checkbox" />
+            item 1
+          </label>
+          <button>delete</button>
+        </li>
+      </ul>
     </>
-  )
+  );
 }
-
-export default App
