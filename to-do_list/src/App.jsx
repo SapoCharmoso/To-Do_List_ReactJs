420c6e9dbef42f76b06bade3c37150da67bd1185
SapoCharmoso <charmyfrogy@gmail.com>
2025-07-03 14:39:35 -0300

    Feito a criação de tarefas e exclusão

--- to-do_list/src/App.jsx
+++ to-do_list/src/App.jsx
@@ -9,10 +9,35 @@ export default function App() {
   function handleSubmit(e) {
     e.preventDefault();
 
-    setTodos(...[todos], {
-      id: crypto.randomUUID,
-      title: newItem,
-      completed: false,
+    setTodos((currentTodos) => {
+      return [
+        ...currentTodos,
+        {
+          id: crypto.randomUUID(),
+          title: newItem,
+          completed: false,
+        },
+      ];
+    });
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos((currentTodos) => {
+      return currentTodos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed };
+        }
+
+        return todo;
+      });
+    });
+    teste;
+  }
+
+  function deleteTodo(id) {
+    console.log(id);
+    setTodos((currentTodos) => {
+      return currentTodos.filter((todo) => todo.id !== id);
     });
   }
   return (
@@ -30,13 +55,22 @@ export default function App() {
       </form>
       <h1 className="">Todo List</h1>
       <ul>
-        <li>
-          <label htmlFor="">
-            <input type="checkbox" />
-            item 1
-          </label>
-          <button>delete</button>
-        </li>
+        {todos.length === 0 && "No Todos"}
+        {todos.map((todo) => {
+          return (
+            <li key={todo.id}>
+              <label>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={(e) => toggleTodo(todo.id, e.target.checked)}
+                />
+                {todo.title}
+              </label>
+              <button onClick={() => deleteTodo(todo.id)}>delete</button>
+            </li>
+          );
+        })}
       </ul>
     </>
   );
